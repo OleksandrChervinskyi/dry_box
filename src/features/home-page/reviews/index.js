be9e2847954677e4f209a5f5e0a93a50b412db85
oleksandr <oleksandrchervinskyi2@gmail.com>
2021-11-08 16:00:29 +0200

    fix reviews

--- src/features/home-page/reviews/index.js
+++ src/features/home-page/reviews/index.js
@@ -4,6 +4,7 @@ import styles from './reviews.module.css'
 import face1 from '../../../face1.png'
 import face2 from '../../../face2.png'
 import face3 from '../../../face3.png'
+import {Link} from "react-router-dom";
 
 
 export const Reviews = () => {
@@ -23,7 +24,9 @@ export const Reviews = () => {
                 <Row>
                     <Col md={4}>
                         <div className={styles.one_review}>
-                            <img src={face1} alt="women1"/>
+                            <Link to={'#'}>
+                                <img src={face1} alt="women1"/>
+                            </Link>
                             <h3>
                                 {name}
                             </h3>
@@ -34,18 +37,23 @@ export const Reviews = () => {
                     </Col>
                     <Col md={4}>
                         <div className={styles.one_review}>
-                            <img src={face2} alt="women1"/>
+                            <Link to={'#'}>
+                                <img src={face2} alt="women2"/>
+                            </Link>
                             <h3>
                                 {name}
                             </h3>
                             <p>
                                 {body}
                             </p>
+
                         </div>
                     </Col>
                     <Col md={4}>
                         <div className={styles.one_review}>
-                            <img src={face3} alt="women1"/>
+                            <Link to={'#'}>
+                                <img src={face3} alt="women3"/>
+                            </Link>
                             <h3>
                                 {name}
                             </h3>
